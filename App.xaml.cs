3f95c9849f8e7468b449426fbfd9e93dc81eb009
sandeepksharma15 <sandeepksharma15@gmail.com>
2024-10-01 09:57:02 +0530

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Cubicon;
+
+public partial class App : System.Windows.Application
+{
+    private NotifyIcon trayIcon;
+    private ContextMenuStrip trayMenu;
+    public static string IconsFolder => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources");
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        // Restore window state
+        // RestoreWindowState();
+
+        DisplayTrayMenu();
+    }
+
+    private void DisplayTrayMenu()
+    {
+        trayMenu = new ContextMenuStrip();
+        trayMenu.Items.Add("About", null, AboutIcoBox);
+        trayMenu.Items.Add("New Icon Box", null, CreateIconGrpup);
+        trayMenu.Items.Add("Start with Windows", null, LoadAtStartup);
+        trayMenu.Items.Add("-");
+        trayMenu.Items.Add("Exit", null, OnExitClick);
+
+        // Set the checked state
+        UpdateStartupMenuCheckState();
+
+        // Create a tray icon
+        trayIcon = new NotifyIcon();
+        trayIcon.Text = "Icon Box";
+        trayIcon.Icon = new Icon(Path.Combine(IconsFolder!, "IcoBox.ico"), 40, 40);
+
+        // Add menu to tray icon
+        trayIcon.ContextMenuStrip = trayMenu;
+
+        // Show the tray icon
+        trayIcon.Visible = true;
+    }
+
+    private void UpdateStartupMenuCheckState()
+    {
+        var loadAtStartupMenuItem = (ToolStripMenuItem)trayMenu!.Items[2];
+        loadAtStartupMenuItem.Checked = Helpers.IsInStartup(AppInfo.AppName);
+    }
+
+    private void LoadAtStartup(object sender, EventArgs e)
+    {
+        //    if (Helpers.IsInStartup(AppInfo.AppName))
+        //        Helpers.RemoveFromStartup(AppInfo.AppName);
+        //    else
+        //        Helpers.AddToStartup(AppInfo.AppName, Application.ExecutablePath);
+
+        //    // Set the checked state
+        //    UpdateStartupMenuCheckState();
+    }
+
+    private void CreateIconGrpup(object sender, EventArgs e)
+    {
+        // new IconBox().Show();
+    }
+
+    private void AboutIcoBox(object sender, EventArgs e)
+    {
+        System.Windows.MessageBox.Show("Show About Box");
+    }
+
+    // Exit action
+    private void OnExitClick(object sender, EventArgs e)
+    {
+        // SaveWindowState();
+
+        trayIcon.Visible = false;
+        Shutdown();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        trayIcon.Dispose();
+        base.OnExit(e);
+    }
+}
---
+++ AppInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Cubicon;
+
+public static class AppInfo
+{
+    public static string AppName { get; set; } = "Cubicon";
+    public static string AppVersion { get; set; } = "0.9.0";
+    public static string AppAuthor { get; set; } = "Sandeep";
+    public static string AppDescription { get; set; } = "A Desktop Icon Organizer";
+    public static string AppLicense { get; set; } = "MIT";
+    public static string AppLicenseVersion { get; set; } = "2.0";
+    public static string AppLicenseAuthor { get; set; } = "Sandeep";
+    public static string AppLicenseUrl { get; set; } = "https://opensource.org/licenses/MIT";
+    public static string AppUrl { get; set; } = "";
+    public static string AppIcon { get; set; } = "";
+
+    public static string NewBoxTitle { get; set; } = "New Cubicon";
+    public static string SaveFileName { get; set; } = "CubiconData.json";
+}
